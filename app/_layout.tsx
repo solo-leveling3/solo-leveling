fded8bc3e98b30aab784d1a048055e07366e1a76
V V SATYA PAVAN SANDEEP PENTAPATI <pspvvsandeep@gmail.com>
2025-07-22 15:20:50 +0530

    Third Version

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,17 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
 
 import { AppProvider } from '@/contexts/AppContext';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { initDatabase } from '@/services/database';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    initDatabase().catch(err => {
-      console.error("Failed to initialize database", err);
-    });
-  }, []);
 
   if (!loaded) {
     // Async font loading only occurs in development.
